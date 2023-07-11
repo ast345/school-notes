ab43c1a210ed406cbd18687f8b2e800593048e5c
小川敦司 <>
2023-07-12 07:33:34 +0900

    usertypeモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_05_120424) do
+ActiveRecord::Schema.define(version: 2023_07_11_222125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,12 @@ ActiveRecord::Schema.define(version: 2023_07_05_120424) do
 
   create_table "subjects", force: :cascade do |t|
     t.string "subject_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "user_types", force: :cascade do |t|
+    t.string "type_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
