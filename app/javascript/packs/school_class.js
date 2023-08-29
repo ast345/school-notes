8f7b5ea7eabf7692f05e067f23c7593911c4fc17
小川敦司 <>
2023-08-29 21:02:28 +0900

    持ち物追加機能を別ファイルへ移動

--- app/javascript/packs/school_class.js
+++ app/javascript/packs/school_class.js
@@ -4,8 +4,9 @@ import { csrfToken } from 'rails-ujs'
 import { dragDropLesson } from './dragdropschedule';
 import { createLesson} from './create_lesson.js';
 import { editLesson } from './edit_lesson.js';
-import { copyPasteLesson} from './copyPasteLesson.js';
-import { event} from './event.js'
+import { copyPasteLesson } from './copyPasteLesson.js';
+import { event } from './event.js'
+import { dateItem } from './date_item.js'
 
 axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
 
@@ -18,111 +19,7 @@ document.addEventListener('turbolinks:load', () =>{
     copyPasteLesson(schoolClassId);
 
     event(schoolClassId);
-
-    // 持ち物の追加
-    $('.item_create_btn').each(function(index, element){
-        const Id = element.id
-        const dataSet = $(element).data()
-        var date = dataSet.date
-        var dayOfWeek = dataSet.dayOfWeek
-
-        $(`#${Id}.item_create_btn`).on('click', () =>{
-            $(`#${Id}.item_btn_box`).addClass('hidden')
-            $(`#${Id}.item_text_box`).removeClass('hidden')
-
-            function createDateItemEndHandler(event) {
-                var clickedElement = event.target;
-                var creatingElement = $(`#${Id}.item_box`);
-                if(!creatingElement.is(clickedElement) && creatingElement.has(clickedElement).length === 0){
-                    var newItem = $(`#item_text${Id}`).val();
-                    if (!newItem) {
-                        $(`#${Id}.item_btn_box`).removeClass('hidden')
-                        $(`#${Id}.item_text_box`).addClass('hidden')
-                        document.removeEventListener('click', createDateItemEndHandler);
-                    } else {
-                        axios.post(`/school_classes/${schoolClassId}/date_items`, {
-                            item: {date: date, day_of_week: dayOfWeek, item_name: newItem}
-                        })
-                        .then((res) =>{
-                            if(res.status === 200){
-                                $(`#item_display${Id}`).removeClass('hidden')
-                                $(`#${Id}.item_text_box`).addClass('hidden')
-                                const itemDisplay = document.getElementById(`item_display${Id}`)
-                                itemDisplay.innerHTML = `${res.data.item_name}`
-                                itemDisplay.setAttribute('data-item-id', `${res.data.id}`)
-                            }
-                        });
-                        document.removeEventListener('click', createDateItemEndHandler);
-                    };
-                };
-            };
-
-            document.addEventListener('click', createDateItemEndHandler);
-        });
-    });
-
-    //行事予定の編集
-    $(`.item_display`).each(function(index, element){
-        const dataSet = $(element).data()
-        const Id = dataSet.id
-        var itemId = dataSet.dateItemId
-        const itemDisplay = document.getElementById(`item_display${Id}`)
-        //datasetが追加されたことを検知して再定義
-        var observer = new MutationObserver(function(mutationsList) {
-            for (var mutation of mutationsList) {
-                if (mutation.type === 'attributes' && mutation.attributeName === 'data-item-id') {
-                    itemId = Number(itemDisplay.getAttribute('data-item-id'))
-                }
-            }
-        });
-
-        observer.observe(itemDisplay, { attributes: true})
-        
-        $(`#item_display${Id}`).on('click', () => {
-            $(this).addClass('hidden')
-            $(`#${Id}.item_text_box`).removeClass('hidden')
-
-
-
-            function editDateItemEndHandler(event) {
-                var clickedElement = event.target;
-                var creatingElement = $(`#${Id}.item_box`);
-                if(!creatingElement.is(clickedElement) && creatingElement.has(clickedElement).length === 0){
-                    var editItem = $(`#item_text${Id}`).val();
-                    if (!editItem) {
-                        axios.delete(`/school_classes/${schoolClassId}/date_items/${itemId}`)
-                        .then((res) =>{
-                            if(res.status === 204){
-                                $(`#${Id}.item_btn_box`).removeClass('hidden')
-                                $(`#${Id}.item_text_box`).addClass('hidden')
-                                document.removeEventListener('click', editDateItemEndHandler);
-                            };
-                        });
-                    } else {
-                        axios.patch(`/school_classes/${schoolClassId}/date_items/${itemId}`, {
-                            item: {item_name: editItem}
-                        })
-                        .then((res) =>{
-                            if(res.status === 200){
-                                $(`#item_display${Id}`).removeClass('hidden')
-                                $(`#${Id}.item_text_box`).addClass('hidden')
-
-                                itemDisplay.innerHTML = `${res.data.item_name}`
-                            };
-                        });
-                        document.removeEventListener('click', editDateItemEndHandler);
-                    };
-
-                };
-
-            };
-
-            document.addEventListener('click', editDateItemEndHandler);
-        });
-    });
-
-
-
+    dateItem(schoolClassId);
 
     //lessonのdestroy機能
     $('.delete_lesson_btn').each(function(index, element){
