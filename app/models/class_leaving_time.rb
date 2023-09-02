b56ecc3d3b8b514eb476054b55f0bb9bf89450fb
小川敦司 <>
2023-09-02 13:13:29 +0900

    曜日情報が正しく保存されていなかった問題を解決

--- app/models/class_leaving_time.rb
+++ app/models/class_leaving_time.rb
@@ -17,6 +17,7 @@
 #
 class ClassLeavingTime < ApplicationRecord
     belongs_to :school_class
-
+    enum day_of_week: { sunday: 0, monday: 1, tuesday: 2, wednesday: 3, thursday: 4, friday: 5, saturday: 6 }
+    
     validates :date, uniqueness: { scope: :school_class_id, message: "この日の行事予定はすでに作成されています。" }
 end
--- app/models/date_item.rb
+++ app/models/date_item.rb
@@ -16,5 +16,6 @@
 #  index_date_items_on_school_class_id           (school_class_id)
 #
 class DateItem < ApplicationRecord
+    enum day_of_week: { sunday: 0, monday: 1, tuesday: 2, wednesday: 3, thursday: 4, friday: 5, saturday: 6 }
     belongs_to :school_class
 end
