416f102fec06d16e321c5a87bc4b1018ca388f35
小川敦司 <>
2023-10-09 15:11:52 +0900

    朝活動のviewに対する表示

--- app/controllers/template_lessons_controller.rb
+++ app/controllers/template_lessons_controller.rb
@@ -4,6 +4,8 @@ class TemplateLessonsController < ApplicationController
         @school_class = SchoolClass.find(params[:school_class_id])
         gon.school_class_id = @school_class.id
         @template_lessons = @school_class.template_lessons
+        @template_morning_acts = @school_class.template_morning_activities
+        @template_date_items = @school_class.template_date_items
         @start_of_week = params[:start_of_week]
 
         @japanese_weekdays = ["日", "月", "火", "水", "木", "金", "土"]
