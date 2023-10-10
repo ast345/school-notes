49bcdbfa127deef72d5f25c0ec85f2fd5d3eef6b
小川敦司 <>
2023-10-10 19:58:19 +0900

    大元のjsファイル名を変更

--- app/javascript/packs/template_schedule.js
+++ app/javascript/packs/template_schedule.js
@@ -4,6 +4,7 @@ import { csrfToken } from 'rails-ujs'
 import {tempLessonCUD} from './temp_lesson_cud'
 import {copyPasteTemplateLesson} from './tempLessonCP'
 import {dragDropTempLesson} from './temp_lesson_dd.js'
+import {tempMorningActivity} from './temp_morning_act.js'
 
 axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
 
@@ -27,6 +28,7 @@ document.addEventListener('turbolinks:load', () =>{
     tempLessonCUD(schoolClassId)
     copyPasteTemplateLesson(schoolClassId)
     dragDropTempLesson(schoolClassId)
+    tempMorningActivity(schoolClassId)
 
 });
 
