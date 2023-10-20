f8aba2305a08ca0b332648d86196007aca3b34d5
小川敦司 <>
2023-10-20 23:52:37 +0900

    ユーザ登録と同時にteacherも自動登録

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" , registrations: "users/registrations"}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get '/' => 'home#index'
-
+  # devise_for :users, :controllers => {
+  #   :registrations => 'users/registrations'
+  #  }
   root to: "home#index"
 
   resources :teachers, only: [:index, :show, :new, :edit, :create, :update]
