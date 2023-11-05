999aadfed6762a5a828f4698f83cbdd0412783d6
小川敦司 <>
2023-11-05 18:23:19 +0900

    updateアクションで専科の単元名に対応

--- app/serializers/lesson_serializer.rb
+++ app/serializers/lesson_serializer.rb
@@ -17,5 +17,5 @@
 #  index_lessons_on_grade_subject_unit_id  (grade_subject_unit_id)
 #
 class LessonSerializer < ActiveModel::Serializer
-    attributes :id, :grade_subject_unit_id, :grade_subject_id
+    attributes :id, :grade_subject_unit_id, :grade_subject_id, :unit_name, :grade_subject_name
 end
