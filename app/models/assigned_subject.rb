775bec44c789921236eb242d18e8d6c2157969f8
小川敦司 <>
2023-11-05 16:35:56 +0900

    候補として表示される教科名を専科の場合には学年もセットで表示されるようにした

--- app/models/assigned_subject.rb
+++ app/models/assigned_subject.rb
@@ -16,4 +16,16 @@
 class AssignedSubject < ApplicationRecord
     belongs_to :school_class_teacher, foreign_key: 'school_class_teachers_id'
     belongs_to :grade_subject, foreign_key: 'grade_subjects_id'
+
+    def subject_display_name
+        grade_id = self.school_class_teacher.school_class.grade.id
+        if grade_id == 13 || grade_id == 14 || grade_id == 15
+            grade_subject = self.grade_subject
+            grade_name = grade_subject.grade.grade_name.gsub('生', '')
+            subject_name = grade_subject.subject.subject_name
+            grade_name + subject_name
+        else
+            self.grade_subject.subject.subject_name
+        end
+    end
 end
