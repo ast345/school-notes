98ae5fd9213adf0d8596d8a85058e1ac67df64b0
小川敦司 <>
2023-11-05 18:35:44 +0900

    assigned_subjectも１、２年生に対応

--- app/models/assigned_subject.rb
+++ app/models/assigned_subject.rb
@@ -19,13 +19,15 @@ class AssignedSubject < ApplicationRecord
 
     def subject_display_name
         grade_id = self.school_class_teacher.school_class.grade.id
-        if grade_id == 13 || grade_id == 14 || grade_id == 15
+        if grade_id == 1 or grade_id == 2
+            grade_subject.subject.yomigana
+        elsif grade_id == 13 || grade_id == 14 || grade_id == 15
             grade_subject = self.grade_subject
             grade_name = grade_subject.grade.grade_name.gsub('生', '')
             subject_name = grade_subject.subject.subject_name
             grade_name + subject_name
         else
-            self.grade_subject.subject.subject_name
+            grade_subject.subject.subject_name
         end
     end
 end
