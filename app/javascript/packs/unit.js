26b0e00800bd47f87a3f514aac442d5cba8d6241
小川敦司 <>
2023-11-10 16:29:41 +0900

    edit処理を実装

---
+++ app/javascript/packs/unit.js
@@ -0,0 +1,26 @@
+import $ from 'jquery'
+import axios from 'axios'
+import { csrfToken } from 'rails-ujs'
+
+axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
+
+// app/assets/javascripts/grade_subject_units.js
+document.addEventListener("turbolinks:load", function() {
+    $('[data-remote="true"]').submit(function(e) {
+      e.preventDefault();
+      $.ajax({
+        type: $(this).attr('method'),
+        url: $(this).attr('action'),
+        data: $(this).serialize(),
+        success: function(data) {
+            var Id = data.id
+            var unitName= document.getElementById(`${Id}`)
+            unitName.innerHTML = `${data.unit_name}`
+        },
+        error: function() {
+            window.alert("単元名を変更できませんでした")
+        }
+      });
+    });
+  });
+  
