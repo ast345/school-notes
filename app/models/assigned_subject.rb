bdf08ff88fa38fb2267b4fc6c463c5572aba14a8
小川敦司 <>
2023-11-18 19:09:43 +0900

    subject_nameが取得できない問題に対処

--- app/models/assigned_subject.rb
+++ app/models/assigned_subject.rb
@@ -20,6 +20,7 @@ class AssignedSubject < ApplicationRecord
     def subject_display_name
         grade_id = self.school_class_teacher.school_class.grade.id
         if grade_id == 1 or grade_id == 2
+            grade_subject = self.grade_subject
             grade_subject.subject.yomigana
         elsif grade_id == 13 || grade_id == 14 || grade_id == 15
             grade_subject = self.grade_subject
@@ -27,6 +28,7 @@ class AssignedSubject < ApplicationRecord
             subject_name = grade_subject.subject.subject_name
             grade_name + subject_name
         else
+            grade_subject = self.grade_subject
             grade_subject.subject.subject_name
         end
     end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -25,6 +25,7 @@ class Lesson < ApplicationRecord
 
     def grade_subject_name(grade_id)
         if grade_id == 1 or grade_id == 2
+            grade_subject = self.grade_subject
             grade_subject.subject.yomigana
         elsif grade_id == 13 || grade_id == 14 || grade_id == 15
             grade_subject = self.grade_subject
@@ -32,6 +33,7 @@ class Lesson < ApplicationRecord
             subject_name = grade_subject.subject.subject_name
             grade_name + subject_name
         else
+            grade_subject = self.grade_subject
             grade_subject.subject.subject_name
         end
     end
