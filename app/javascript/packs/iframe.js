8be70e7fee2ca7da3b5b5131f0af15e0c0a92134
小川敦司 <>
2023-12-03 21:30:51 +0900

    shareページにも教科名と単元名の文字の大きさ調整機能を実装

--- app/javascript/packs/iframe.js
+++ app/javascript/packs/iframe.js
@@ -46,4 +46,47 @@ document.addEventListener('turbolinks:load', () =>{
     $('.item_display').each(function(index, element){
         adjustItemFontSize(element);
     })
+
+    // 教科名のフォントサイズ調整
+    function adjustSubjectFZ(element) {
+        const $element = $(element);
+        const rowHeight = $('.row_lesson').height()/5*2 ;  // 要素の高さを取得
+        const originalHTML = $element.html(); // 元のHTMLを保持
+        let fontSize = parseInt($element.css('font-size')); // デフォルトのフォントサイズを取得
+        let lineHeight = parseInt($element.css('line-height'));
+        
+        while (($element[0].scrollHeight > rowHeight || $element[0].getClientRects().length > 1) && fontSize > 1) {
+            fontSize -= 1; // フォントサイズを1ずつ減らす（必要に応じて調整可能）
+            $element.css({
+                'font-size': fontSize + 'px',
+                'line-height': lineHeight + 'px',
+            });
+        }
+        $element.html(originalHTML);
+    }
+
+    function adjustUnitFZ(element) {
+        const $element = $(element);
+        const rowHeight = $('.row_lesson').height()/5*3 ; // 要素の高さを取得
+        const originalHTML = $element.html(); // 元のHTMLを保持
+        let fontSize = parseInt($element.css('font-size')); // デフォルトのフォントサイズを取得
+        let lineHeight = parseInt($element.css('line-height')); // 行の高さを取得
+        $element.css('white-space', 'normal'); // テキストを通常の折り返しに設定
+        while ($element[0].scrollHeight > rowHeight  && fontSize > 1) {
+            fontSize -= 1; // フォントサイズを1ずつ減らす（必要に応じて調整可能）
+            lineHeight = Math.floor(fontSize * 1.2); // 行の高さも変更（フォントサイズに基づいて調整）
+            $element.css({
+                'font-size': fontSize + 'px',
+                'line-height': lineHeight + 'px',
+            });
+        }
+        $element.html(originalHTML);
+    }
+
+    $('.lesson_subject').each(function(index, element){
+        adjustSubjectFZ(element);
+    })
+    $('.lesson_unit').each(function(index, element){
+        adjustUnitFZ(element);
+    })
 });
