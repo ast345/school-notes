c3cbaa7ca8370d50d9d3a40f3851ade8b54d2c05
小川敦司 <>
2024-01-08 21:00:41 +0900

    ゲストログイン機能を追加

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,12 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
+  def new_guest
+    user = User.find_by(email: 'guest@example.com')
+    sign_in user, scope: :user
+    redirect_to after_sign_in_path_for(user), notice: 'ゲストユーザーとしてログインしました。'
+  end
+  
   # GET /resource/sign_in
   # def new
   #   super
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" , registrations: "users/registrations"}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get '/' => 'home#index'
-  # devise_for :users, :controllers => {
-  #   :registrations => 'users/registrations'
-  #  }
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   root to: "home#index"
 
   resources :teachers, only: [:update]
